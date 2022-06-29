f43716ca002f03cbad9804c505d4800bc926f42a
Miroslav Mrňák <miroslav.mrnak@gmail.com>
2022-06-29 20:41:14 +0200

    ToDo comments added

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from functools import lru_cache
 from microbit import sleep, button_a, button_b, display
 from microbit import Image
 from LED import carON
 from modes import mode0, mode1, mode2
 
 mode = 0
+
+#TODO
+#zde definovat globalni proměné rychlosti RF(RightFront), RR(RightRear), LF, LR
+#ty se budou předavat volaným modum, napr. mode1(RF, RR, LF, LR)
+#v modulu motors bude zakomponovaná fce počítadlo / rychlost motoru, které bude vyhodnocovat, který motor přibrzdí
+
 
 sleep(500)
 display.show(Image.ALL_CLOCKS, loop=False, delay=100)
